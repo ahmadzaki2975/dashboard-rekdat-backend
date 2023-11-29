c754d5aa23d44242d6cac52f6a2958f0c79fb845
Ahmad Zaki Akmal <87590846+ahmadzaki2975@users.noreply.github.com>
2023-11-29 22:14:18 +0700

    fix: prevent injections, disable bmkg and iqair route

--- index.js
+++ index.js
@@ -1,87 +1,114 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 5000;
-const knex = require('knex');
+const knex = require("knex");
 
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 
-const morgan = require('morgan');
-app.use(morgan('dev'));
+const morgan = require("morgan");
+app.use(morgan("dev"));
 
-const cors = require('cors');
-app.use(cors({
-  origin: '*'
-}));
+const cors = require("cors");
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 const db = knex({
-  client: 'pg',
+  client: "pg",
   connection: {
     host: process.env.DB_HOST,
-    user: process.env.DB_USER, 
+    user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: 5432
-  }
+    port: 5432,
+  },
 });
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.get('/bmkg', (req, res) => {
-  const {sdate, edate, city} = req.query;
+// app.get("/bmkg", (req, res) => {
+//   const { sdate, edate, city } = req.query;
+//   // format from YYYY-MM-DD date to utc
+//   const start_date = new Date(sdate).toISOString();
+//   const end_date = new Date(edate).toISOString();
+//   console.log(start_date, end_date);
+//   try {
+//     db.select("*")
+//       .from("bmkg")
+//       .whereBetween("jamcuaca", [start_date, end_date])
+//       .andWhere("kota", city)
+//       .then((data) => {
+//         res.json(data);
+//       });
+//   } catch (err) {
+//     console.log(err);
+//   }
+// });
+
+// app.get("/iqair", (req, res) => {
+//   const { sdate, edate, city } = req.query;
+//   // format from YYYY-MM-DD date to utc
+//   const start_date = new Date(sdate).toISOString();
+//   const end_date = new Date(edate).toISOString();
+//   console.log(start_date, end_date, city);
+//   if (!city) {
+//     return res.send("Please specify city");
+//   }
+//   try {
+//     db.select("*")
+//       .from("iqair")
+//       .whereBetween("accessed", [start_date, end_date])
+//       .andWhere("kota", city)
+//       .then((data) => {
+//         res.json(data);
+//       });
+//   } catch (err) {
+//     console.log(err);
+//   }
+// });
+
+app.get("/join", (req, res) => {
+  const { sdate, edate, city } = req.query;
   // format from YYYY-MM-DD date to utc
+  // prevent injections
+  if (
+    sdate.includes("'") ||
+    edate.includes("'") ||
+    city.includes("'") ||
+    sdate.includes(";") ||
+    edate.includes(";") ||
+    city.includes(";")
+  ) {
+    return res.send("Mau SQL injection ya mas?");
+  }
+  if (!sdate || !edate) {
+    return res.send("Please specify start date and end date");
+  }
   const start_date = new Date(sdate).toISOString();
   const end_date = new Date(edate).toISOString();
-  console.log(start_date, end_date);
-  try{
-    db.select('*').from('bmkg').whereBetween(
-      'jamcuaca', [start_date, end_date]
-    ).andWhere('kota', city).then(data => {
-      res.json(data);
-    });
-  }
-  catch(err){
-    console.log(err);
-  }
-});
 
-app.get('/iqair', (req, res) => {
-  const {sdate, edate, city} = req.query;
-  // format from YYYY-MM-DD date to utc
-  const start_date = new Date(sdate).toISOString();
-  const end_date = new Date(edate).toISOString();
-  console.log(start_date, end_date, city);
-  if(!city){
+  if (!city) {
     return res.send("Please specify city");
   }
-  try{
-    db.select('*').from('iqair').whereBetween('accessed', [start_date, end_date]).andWhere('kota', city).then(data => {
-      res.json(data);
-    });
-  }
-  catch(err){
+  console.log(start_date, end_date);
+
+  try {
+    db.select("*")
+      .from("join_table")
+      .whereBetween("jamcuaca", [start_date, end_date])
+      .andWhere("kota", city)
+      .then((data) => {
+        res.json(data);
+      });
+  } catch (err) {
     console.log(err);
   }
 });
-
-app.get('/join', (req, res) => {
-  const {sdate, edate, city} = req.query;
-  // format from YYYY-MM-DD date to utc
-  const start_date = new Date(sdate).toISOString();
-  const end_date = new Date(edate).toISOString();
-  console.log(start_date, end_date);
-
-  try{
-    db.select('*').from('join_table').whereBetween('jamcuaca', [start_date, end_date]).andWhere('kota', city).then(data => {
-      res.json(data);
-    });
-  }
-  catch(err){
-    console.log(err);
-  }
-})
 
 app.listen(port, () => {
   console.log(`Backend listening at http://localhost:${port}`);
